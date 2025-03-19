a3cc899f1439d3997ce29bfd6069e6b6081790a5
konglong147 <freekonglong@163.com>
2025-03-19 04:00:43 -0700

    ss

---
+++ cmd/gobind/implicit.go
@@ -0,0 +1,11 @@
+// This file imports implicit dependencies required by generated code.
+
+//go:build mobile_implicit
+
+package main
+
+import (
+	_ "github.com/sagernet/gomobile/bind"
+	_ "github.com/sagernet/gomobile/bind/java"
+	_ "github.com/sagernet/gomobile/bind/objc"
+)
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/sagernet/gomobile
+
+go 1.18
+
+require (
+	golang.org/x/mod v0.14.0
+	golang.org/x/sync v0.5.0
+	golang.org/x/tools v0.16.0
+)
